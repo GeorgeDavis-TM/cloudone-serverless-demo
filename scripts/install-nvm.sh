165cd40ffe6de687e3167f3f2719de0b3a2bc2fe
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-02-17 19:04:31 -0500

    Use fix from nvm issue #1837

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -2,13 +2,13 @@
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
 mv install.sh nvm-install.sh
 chmod +x nvm-install.sh
+touch ~/.bash_profile
 ./nvm-install.sh
-whoami
-pwd
+cat ~/.bash_profile
 cat ~/.bashrc
 source ~/.bashrc
-source ~/.bashrc && nvm --version
-source ~/.bashrc && nvm install 14
-source ~/.bashrc && nvm ls
-source ~/.bashrc && nvm use 14
-source ~/.bashrc && nvm current
+nvm --version
+nvm install 14
+nvm ls
+nvm use 14
+nvm current

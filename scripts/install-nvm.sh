5120c9f5f895547f328ec57d5294682c36f2cda3
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-02-18 11:07:51 -0500

    Adding lines to the nvm install script

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -2,11 +2,9 @@
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
 mv install.sh nvm-install.sh
 chmod +x nvm-install.sh
-touch ~/.bash_profile
+./nvm-install.sh | tail -n 2 >> /home/ec2-user/.bash_profile
 ./nvm-install.sh
-cat ~/.bash_profile
-cat ~/.bashrc
-source ~/.bashrc
+source /home/ec2-user/.bash_profile
 nvm --version
 nvm install 14
 nvm ls

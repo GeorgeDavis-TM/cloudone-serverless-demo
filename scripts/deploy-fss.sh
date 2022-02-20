23c5d81e608a54376be8dad99e978013750fadd1
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-02-20 02:52:45 -0500

    debug test for fss

--- scripts/deploy-fss.sh
+++ scripts/deploy-fss.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /home/ec2-user
 yum install -y git wget > ~/deploy-fss.log 2>&1
-git clone https://github.com/GeorgeDavis-TM/filestoragesecurity-malwaretest.git >> ~/deploy-fss.log 2>&1
-cd filestoragesecurity-malwaretest
-source /home/ec2-user/.bash_profile
-nvm use 14 >> ~/deploy-fss.log 2>&1
-npm install -g serverless >> ~/deploy-fss.log 2>&1
-serverless --version >> ~/deploy-fss.log 2>&1
-serverless plugin install -n serverless-python-requirements # >> ~/deploy-fss.log 2>&1
 VERSION=v4.20.2
 BINARY=yq_linux_amd64
 wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && mv ${BINARY} /usr/bin/yq >> ~/deploy-fss.log 2>&1
+git clone https://github.com/GeorgeDavis-TM/filestoragesecurity-malwaretest.git >> ~/deploy-fss.log 2>&1
+cd filestoragesecurity-malwaretest
 yq -i '.custom.stages.prod.profile = "default"' serverless.yml
+source /home/ec2-user/.bash_profile
+nvm use 14 >> ~/deploy-fss.log 2>&1
+npm install -g serverless # >> ~/deploy-fss.log 2>&1
+# serverless --version >> ~/deploy-fss.log 2>&1
+serverless plugin install -n serverless-python-requirements # >> ~/deploy-fss.log 2>&1
 serverless deploy -s prod # >> ~/deploy-fss.log 2>&1

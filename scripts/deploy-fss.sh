0cae753ee6a6d045d53d18c3853e5c6420457875
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-02-20 00:57:09 -0500

    debug test for fss

--- scripts/deploy-fss.sh
+++ scripts/deploy-fss.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 cd /home/ec2-user
+yum install -y git wget > ~/deploy-ws.log 2>&1
 git clone https://github.com/GeorgeDavis-TM/filestoragesecurity-malwaretest.git
 cd filestoragesecurity-malwaretest
 source /home/ec2-user/.bash_profile
-sls deploy
+nvm ls
+nvm use 14
+nvm current
+npm install -g serverless
+sls --version
+VERSION=v4.20.2
+BINARY=yq_linux_amd64
+wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && mv ${BINARY} /usr/bin/yq
+yq -i '.custom.stages.prod.profile = "default"' serverless.yml
+sls deploy -s prod

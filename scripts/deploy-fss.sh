995aad7f95320d70186c4f9f7e309faf7ff730ac
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-14 18:54:00 -0400

    Adding YAML parsing to configure serverless.yml file

--- scripts/deploy-fss.sh
+++ scripts/deploy-fss.sh
@@ -3,7 +3,9 @@ cd /home/ec2-user
 yum install -y git wget > ~/deploy-fss.log 2>&1
 VERSION=v4.20.2
 BINARY=yq_linux_amd64
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && mv ${BINARY} /usr/bin/yq >> ~/deploy-fss.log 2>&1
+wget -P /home/ec2-user -O ${BINARY}.tar.gz https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz >> ~/deploy-fss.log 2>&1
+tar xvf /home/ec2-user/${BINARY}.tar.gz >> ~/deploy-fss.log 2>&1
+mv /home/ec2-user/${BINARY} /usr/bin/yq >> ~/deploy-fss.log 2>&1
 git clone https://github.com/GeorgeDavis-TM/filestoragesecurity-malwaretest.git >> ~/deploy-fss.log 2>&1
 cd filestoragesecurity-malwaretest
 yq -i '.custom.stages.prod.profile = "default"' serverless.yml

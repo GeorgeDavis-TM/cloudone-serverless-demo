3b7062be70dffe184da3203f902ac4618fa55ba0
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-16 12:25:00 -0400

    Adding jq to the install-dsa.sh script

--- scripts/install-dsa.sh
+++ scripts/install-dsa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WSApiKey=$1
-yum install -y git wget curl
+yum install -y git wget curl jq
 git clone https://github.com/GeorgeDavis-TM/deepsecurity-deploy.git
 cd deepsecurity-deploy
 chmod +x agent_health_status.sh

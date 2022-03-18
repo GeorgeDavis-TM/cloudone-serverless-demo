13e8d8ef33b90b08b1637dfebc70810e32ac14d5
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-17 22:53:47 -0400

    Adding more verbose logging to SSMGetResponseLambdaFunction

--- scripts/python/lambda-get-command-invocation.py
+++ scripts/python/lambda-get-command-invocation.py
@@ -1,8 +1,15 @@
 import os
 import time
 import boto3
+import botocore.exceptions
+import logging
 
 def lambda_handler(event, context):
+
+    logger = logging.getLogger('boto3').setLevel(logging.DEBUG)
+    logging.getLogger('botocore').setLevel(logging.DEBUG)
+
+    logger.info("event: {}".format(event))
     
     regionName = str(os.environ.get("REGION_NAME")) if 'REGION_NAME' in os.environ else None
     sleepTimer = int(os.environ.get("SLEEP_TIMER")) if 'SLEEP_TIMER' in os.environ else 5
@@ -10,59 +17,67 @@ def lambda_handler(event, context):
 
     if regionName:
         
-        commandId = event["Command"]["CommandId"]
-        targetTags = event["Command"]["Targets"]
-        
-        filterTags = []
-        for tagItem in targetTags:
-            filterTags.append({"Name": tagItem["Key"], "Values": tagItem["Values"]})
-        
-        ec2Client = boto3.client('ec2', region_name=regionName)
-        
-        ec2DescribeInstancesResponse = ec2Client.describe_instances(
-            Filters=filterTags,
-            MaxResults=5
-        )
-        
-        # print(str(ec2DescribeInstancesResponse))
-        
-        ec2Instances = ec2DescribeInstancesResponse["Reservations"][0]["Instances"]
-        
-        instanceId = None
-        
-        if len(ec2Instances) == 1:
-            instanceId = ec2Instances[0]["InstanceId"]
-        else:
-            raise Exception("Multiple instances found. Reduce the number of stack(s) deployed and try again.")
+        try:
+            commandId = event["Command"]["CommandId"]
+            targetTags = event["Command"]["Targets"]
+            
+            filterTags = []
+            for tagItem in targetTags:
+                filterTags.append({"Name": tagItem["Key"], "Values": tagItem["Values"]})
+            
+            ec2Client = boto3.client('ec2', region_name=regionName)
+            
+            ec2DescribeInstancesResponse = ec2Client.describe_instances(
+                Filters=filterTags,
+                MaxResults=5
+            )
+            
+            # print(str(ec2DescribeInstancesResponse))
+            
+            ec2Instances = ec2DescribeInstancesResponse["Reservations"][0]["Instances"]
+            
+            instanceId = None
+            
+            if len(ec2Instances) == 1:
+                instanceId = ec2Instances[0]["InstanceId"]
+            else:
+                raise Exception("Multiple instances found. Reduce the number of stack(s) deployed and try again.")
 
-        time.sleep(sleepTimer)
-    
-        ssmClient = boto3.client('ssm', region_name=regionName)        
+            time.sleep(sleepTimer)
         
-        waiter = ssmClient.get_waiter('command_executed')
-        
-        # print(str(commandId), str(instanceId))
+            ssmClient = boto3.client('ssm', region_name=regionName)        
+            
+            waiter = ssmClient.get_waiter('command_executed')
+            
+            # print(str(commandId), str(instanceId))
 
-        waiter.wait(
-            CommandId=commandId,
-            InstanceId=instanceId,
-            WaiterConfig={
-                'Delay': 5,
-                'MaxAttempts': 60
-            }
-        )    
+            waiter.wait(
+                CommandId=commandId,
+                InstanceId=instanceId,
+                WaiterConfig={
+                    'Delay': 5,
+                    'MaxAttempts': 60
+                }
+            )    
 
-        getCommandInvocationResponse = ssmClient.get_command_invocation(
-            CommandId=commandId,
-            InstanceId=instanceId,
-            PluginName=pluginName
-        )
-        
-        print(str(getCommandInvocationResponse))
+            getCommandInvocationResponse = ssmClient.get_command_invocation(
+                CommandId=commandId,
+                InstanceId=instanceId,
+                PluginName=pluginName
+            )
+            
+            print(str(getCommandInvocationResponse))
 
-        if getCommandInvocationResponse["Status"] == "Success":
-            print("Success: " + getCommandInvocationResponse["StandardOutputContent"] + " - " + getCommandInvocationResponse["StandardOutputUrl"])
-            return True
-        else:
-            raise Exception("Error: " + getCommandInvocationResponse["StandardErrorContent"] + " - " + getCommandInvocationResponse["StandardErrorUrl"])
-            return False
+            if getCommandInvocationResponse["Status"] == "Success":
+                print("Success: " + getCommandInvocationResponse["StandardOutputContent"] + " - " + getCommandInvocationResponse["StandardOutputUrl"])
+                return True
+            else:
+                raise Exception("Error: " + getCommandInvocationResponse["StandardErrorContent"] + " - " + getCommandInvocationResponse["StandardErrorUrl"])
+                return False
+
+        except botocore.exceptions.WaiterError as error:
+            print("Error: An unknown exception occurred.", error)            
+            raise error
+            
+        except:
+            print("Error: An unknown exception occurred.")

f3a9ce348a4b9f857ea6b1641c2a36c19d24ccda
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-18 15:25:55 -0400

    Passing C1WS Policy Id to the deployment script

--- scripts/install-dsa.sh
+++ scripts/install-dsa.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 WSApiKey=$1
+WSPolicyId=$2
 yum install -y git wget curl jq
 git clone https://github.com/GeorgeDavis-TM/deepsecurity-deploy.git
 cd deepsecurity-deploy
 chmod +x agent_health_status.sh
-./agent_health_status.sh ${WSApiKey}
+./agent_health_status.sh ${WSApiKey} ${WSPolicyId}

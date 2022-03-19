a549b9b768e2376b613fd9910142915cd322f6ba
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-19 02:29:51 -0400

    Writing logs to files in /home/ec2-user

--- scripts/install-dsa.sh
+++ scripts/install-dsa.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 WSApiKey=$1
 WSPolicyId=$2
-yum install -y git wget curl jq
-git clone https://github.com/GeorgeDavis-TM/deepsecurity-deploy.git
+yum install -y git wget curl jq > /home/ec2-user/install-dsa.log 2>&1
+git clone https://github.com/GeorgeDavis-TM/deepsecurity-deploy.git >> /home/ec2-user/install-dsa.log 2>&1
 cd deepsecurity-deploy
 chmod +x agent_health_status.sh
-./agent_health_status.sh ${WSApiKey} ${WSPolicyId}
+./agent_health_status.sh ${WSApiKey} ${WSPolicyId} >> /home/ec2-user/install-dsa.log 2>&1
--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
+wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh > /home/ec2-user/install-nvm.log 2>&1
 mv install.sh nvm-install.sh
 chmod +x nvm-install.sh
 ./nvm-install.sh | tail -n 2 >> /home/ec2-user/.bash_profile
-./nvm-install.sh
+./nvm-install.sh >> /home/ec2-user/install-nvm.log 2>&1
 source /home/ec2-user/.bash_profile
-nvm --version
-nvm install 14
-nvm ls
-nvm use 14
-nvm current
+nvm --version >> /home/ec2-user/install-nvm.log 2>&1
+nvm install 14 >> /home/ec2-user/install-nvm.log 2>&1
+nvm ls >> /home/ec2-user/install-nvm.log 2>&1
+nvm use 14 >> /home/ec2-user/install-nvm.log 2>&1
+nvm current >> /home/ec2-user/install-nvm.log 2>&1

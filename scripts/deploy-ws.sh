da37dfc8039d9c962f62f2b39d71091d01ab704f
GeorgeDavis-TM <george_davis@trendmicro.com>
2022-03-19 00:00:00 -0400

    Replacing dsm-py-sdk with c1ws-py-sdk, changing logging folder location to /home/ec2-user

--- scripts/deploy-ws.sh
+++ scripts/deploy-ws.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 dsApiKey=$1
 cd /home/ec2-user
-yum install -y python3 python3-pip git curl unzip wget jq > ~/deploy-ws.log 2>&1
-wget -P /home/ec2-user -O dsm-py-sdk.zip https://automation.deepsecurity.trendmicro.com/sdk/20_0/v1/dsm-py-sdk.zip >> ~/deploy-ws.log 2>&1
-unzip -o /home/ec2-user/dsm-py-sdk.zip >> ~/deploy-ws.log 2>&1
-pip3 install . >> ~/deploy-ws.log 2>&1
-git clone https://github.com/GeorgeDavis-TM/cloudOneWorkloadSecurityDemo.git >> ~/deploy-ws.log 2>&1
+yum install -y python3 python3-pip git curl unzip wget jq > /home/ec2-user/deploy-c1ws.log 2>&1
+wget -P /home/ec2-user -O c1ws-py-sdk.zip https://cloudone.trendmicro.com/docs/downloads/sdk/ws/v1/c1ws-py-sdk.zip >> /home/ec2-user/deploy-c1ws.log 2>&1
+unzip -o /home/ec2-user/c1ws-py-sdk.zip >> /home/ec2-user/deploy-c1ws.log 2>&1
+pip3 install . >> /home/ec2-user/deploy-c1ws.log 2>&1
+git clone https://github.com/GeorgeDavis-TM/cloudOneWorkloadSecurityDemo.git >> /home/ec2-user/deploy-c1ws.log 2>&1
 cd cloudOneWorkloadSecurityDemo
-pip3 install -r requirements.txt >> ~/deploy-ws.log 2>&1
-localHostname=`curl http://169.254.169.254/latest/meta-data/local-hostname` >> ~/deploy-ws.log 2>&1
+pip3 install -r requirements.txt >> /home/ec2-user/deploy-c1ws.log 2>&1
+localHostname=`curl http://169.254.169.254/latest/meta-data/local-hostname` >> /home/ec2-user/deploy-c1ws.log 2>&1
 # TAG_NAME="Name"
-# INSTANCE_ID=`curl http://instance-data/latest/meta-data/instance-id` >> ~/deploy-ws.log 2>&1
-# REGION=`curl http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'` >> ~/deploy-ws.log 2>&1
-# TAG_VALUE="`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$TAG_NAME" --region $REGION --output=text | cut -f5`" >> ~/deploy-ws.log 2>&1
+# INSTANCE_ID=`curl http://instance-data/latest/meta-data/instance-id` >> /home/ec2-user/deploy-c1ws.log 2>&1
+# REGION=`curl http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'` >> /home/ec2-user/deploy-c1ws.log 2>&1
+# TAG_VALUE="`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$TAG_NAME" --region $REGION --output=text | cut -f5`" >> /home/ec2-user/deploy-c1ws.log 2>&1
 instance="("$localHostname")"
 dsmPolicyName="Serverless"
 tmp=$(mktemp)

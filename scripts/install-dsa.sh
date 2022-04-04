99a2e748b23f0ddf53290954ae719c9c32433168
George Davis <george_davis@trendmicro.com>
2022-04-04 14:13:03 -0400

    Adding switches to the agent_status_health.sh, adding verbosity

--- scripts/install-dsa.sh
+++ scripts/install-dsa.sh
@@ -5,4 +5,4 @@ yum install -y git wget curl jq > /home/ec2-user/install-dsa.log 2>&1
 git clone https://github.com/GeorgeDavis-TM/deepsecurity-deploy.git >> /home/ec2-user/install-dsa.log 2>&1
 cd deepsecurity-deploy
 chmod +x agent_health_status.sh
-./agent_health_status.sh ${WSApiKey} ${WSPolicyId} >> /home/ec2-user/install-dsa.log 2>&1
+./agent_health_status.sh --apikey ${WSApiKey} --policyid ${WSPolicyId} --verbose >> /home/ec2-user/install-dsa.log 2>&1
